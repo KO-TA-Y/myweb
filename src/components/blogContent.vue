a8106888b3ad6c27fa7204b2e9c6d175f278011e
Kota Yokoyama <kohtafms@gmail.com>
2020-03-07 12:48:59 +0900

    create blog component Blogコンポーネントを作成

---
+++ src/components/blogContent.vue
@@ -0,0 +1,79 @@
+<template>
+  <div id="blogContent">
+    <v-card
+      class="mx-auto"
+      max-width="380"
+      outlined
+      :width="this.$store.state.width*0.8"
+    >
+      <v-img
+        :aspect-ratio="16/9"
+        :src=imgSrc
+      >
+        <template v-slot:placeholder>
+          <v-row
+            class="fill-height ma-0"
+            align="center"
+            justify="center"
+          >
+            <v-progress-circular indeterminate color="grey lighten-2"></v-progress-circular>
+          </v-row>
+        </template>
+      </v-img>
+      <v-divider></v-divider>
+      <v-card-title class="subtitle-1 font-weight-bold">{{title}}</v-card-title>
+      <v-card-text class="text--primary">
+        <div>{{description}}</div>
+      </v-card-text>
+      <v-card-subtitle class="pb-0 pt-2">{{date}}</v-card-subtitle>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn
+          text
+          :href="link"
+          outlined
+        >
+          詳細
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </div>
+</template>
+
+<script>
+  export default {
+    name:'blogContent',
+    data(){
+      return{
+
+      }
+    },
+    props:{
+      imgSrc: {
+        type: String,
+        required: true
+      },
+      title: {
+        type: String,
+        required: true
+      },
+      date: {
+        type: String,
+        required: true
+      },
+      description: {
+        type: String,
+        required: true
+      },
+      link: {
+        type: String,
+        required: true
+      },
+      
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -1,5 +1,60 @@
 <template>
-  <div class="blog">
-    <h1>This is an blog page</h1>
+  <div id="blog" class="grey lighten-4">
+    <v-sheet
+      color="grey lighten-4 mx-auto"
+      tile
+      class="pt-8"
+      max-width=900
+      :min-height="this.$store.state.height"
+      :width="this.$store.state.width"
+    >
+      <v-list v-if="this.$store.state.showBar" min-width=700 max-width=900 :width="this.$store.state.width" class="mx-auto" color="grey lighten-4">
+        <v-list-item  class="pl-12">
+          <v-card-title class="display-1 pl-0 ml-0">Blog</v-card-title>
+        </v-list-item>
+        <v-divider class="mx-5"></v-divider>
+      </v-list>
+      <v-list v-if="!this.$store.state.showBar" min-width=320 max-width=500 :width="this.$store.state.width" class="mx-auto" color="grey lighten-4">
+        <v-list-item  class="pl-12">
+          <v-card-title class="headline pl-0 ml-0 pt-0" >Blog</v-card-title>
+        </v-list-item>
+        <v-divider class="mx-5"></v-divider>
+      </v-list>
+      <v-container max-width=1200>
+        <div class="px-auto mx-auto text-center" v-if="contents.length===0">ブログの記事は現在ありません</div>
+        <v-row class="mx-auto">
+          <v-col
+            v-for="content in contents"
+            :key="content.title"
+            class="mr-auto child-flex"
+            :cols="researchCols"
+          >   
+            <researchContent :imgSrc="content.imgSrc" :title="content.title" :date="content.date" :description="content.desc" :link="content.link"></researchContent>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-sheet>
   </div>
 </template>
+
+<script>
+import researchContent from '../components/blogContent.vue'
+export default {
+  name:'blog',
+  components:{
+    researchContent,
+  },
+  data(){
+    return{
+      researchCols:this.$store.state.researchCols,
+      contents:[
+        
+      ]
+    }
+  },
+  updated() {
+    this.researchCols=this.$store.state.researchCols
+  },
+
+}
+</script>
